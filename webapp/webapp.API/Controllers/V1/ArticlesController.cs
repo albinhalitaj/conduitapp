4db08fdebcc78cfcdb1592d5edb6c2a2e44dc09a
Albin Halitaj <albin.halitaj@gmail.com>
2022-08-18 01:41:53 +0200

    chore: refactor api responses

--- webapp/webapp.API/Controllers/V1/ArticlesController.cs
+++ webapp/webapp.API/Controllers/V1/ArticlesController.cs
@@ -26,14 +26,13 @@ public class ArticlesController : ApiController
     public async Task<IActionResult> GetArticles([FromQuery] QueryParams queryParams)
     {
         var result = await _articleService.GetAllArticlesAsync(queryParams);
-        return !result.Success ? Problem(result.Errors) : result.Value!.Count > 1 ? Ok(new
-        {
-            Articles = result.Value,
-            ArticlesCount = result.Value.Count
-        }) : Ok(new
-        {
-            Article = result.Value
-        });
+        return !result.Success
+            ? Problem(result.Errors)
+            : Ok(new
+            {
+                Articles = result.Value,
+                ArticlesCount = result.Value!.Any() ? result.Value!.Count : 0
+            });
     }
 
     [HttpGet("{slug}")]
@@ -54,86 +53,124 @@ public class ArticlesController : ApiController
     public async Task<IActionResult> Feed([FromQuery] QueryParams queryParams)
     {
         var result = await _articleService.Feed(queryParams);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return !result.Success ? Problem(result.Errors) : Ok(new
+            {
+                Articles = result.Value,
+                ArticlesCount = result.Value!.Any() ? result.Value!.Count : 0
+            });
     }
 
     [HttpGet("byAuthor")]
     public async Task<IActionResult> GetArticleByAuthor([FromQuery] string author)
     {
         var result = await _articleService.GetArticleByAuthorAsync(author);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return !result.Success ? Problem(result.Errors) : Ok(new
+            {
+                Articles = result.Value,
+                ArticlesCount = result.Value!.Any() ? result.Value!.Count : 0
+            });
     }
 
     [HttpGet("byTag")]
     public async Task<IActionResult> GetArticleByTag([FromQuery] string tag)
     {
         var result = await _articleService.GetArticleByTagAsync(tag);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return !result.Success
+            ? Problem(result.Errors)
+            : Ok(new
+            {
+                Articles = result.Value,
+                ArticlesCount = result.Value!.Count
+            });
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateArticle([FromBody] CreateArticleRequest request)
     {
         var result = await _articleService.CreateArticleAsync(request);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? Ok(new
+        {
+            Article = result.Value
+        }) : Problem(result.Errors);
     }
 
     [HttpPut("{slug}")]
     public async Task<IActionResult> UpdateArticle([FromRoute] string slug,UpdateArticleRequest request)
     {
         var response = await _articleService.UpdateArticleAsync(slug, request);
-        return response.Success ? Ok(response.Value) : Problem(response.Errors);
+        return response.Success ? Ok(new
+        {
+            Article = response.Value
+        }) : Problem(response.Errors);
     }
 
     [HttpPost("{slug}/favorite")]
     public async Task<IActionResult> FavoriteArticle([FromRoute] string slug)
     {
         var result = await _articleService.FavoriteArticle(slug);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? Ok(new
+        {
+            Article = result.Value
+        }) : Problem(result.Errors);
     }
 
     [HttpDelete("{slug}/favorite")]
     public async Task<IActionResult> UnFavoriteArticle([FromRoute] string slug)
     {
         var result = await _articleService.UnFavoriteArticle(slug);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? Ok(new
+        {
+            Article = result.Value
+        }) : Problem(result.Errors);
     }
 
     [HttpGet("byFavorite")]
     public async Task<IActionResult> GetArticleByFavorite([FromQuery] string author)
     {
         var result = await _articleService.GetArticleByFavorites(author);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return !result.Success ? Problem(result.Errors) : result.Value!.Count > 1 ? Ok(new
+        {
+            Articles = result.Value,
+            ArticlesCount = result.Value.Count
+        }) : Ok(new
+        {
+            Article = result.Value.First()
+        });
     }
 
     [HttpPost("{slug}/comments")]
     public async Task<IActionResult> CreateComment([FromRoute] string slug,CreateCommentRequest request)
     {
         var result = await _commentService.CreateComment(slug,request);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? Ok(new
+        {
+            Comment = result.Value
+        }) : Problem(result.Errors);
     }
 
     [HttpGet("{slug}/comments")]
     public async Task<IActionResult> ListComments([FromRoute] string slug)
     {
         var result = await _commentService.ListComments(slug);
-        return result.Success ? Ok(new
-        {
-            Comments = result.Value
-        }) : Problem(result.Errors);
+        return !result.Success
+            ? Problem(result.Errors)
+            : Ok(new
+            {
+                Comments = result.Value,
+            });
     }
 
     [HttpDelete("{slug}/comments/{id}")]
     public async Task<IActionResult> DeleteComment([FromRoute] string slug,[FromRoute] string id)
     {
         var result = await _commentService.DeleteCommentAsync(slug,id);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? NoContent() : Problem(result.Errors);
     }
 
     [HttpDelete("{slug}")]
     public async Task<IActionResult> DeleteArticle([FromRoute] string slug)
     {
         var result = await _articleService.DeleteArticleAsync(slug);
-        return result.Success ? Ok(result.Value) : Problem(result.Errors);
+        return result.Success ? NoContent() : Problem(result.Errors);
     }
 }
